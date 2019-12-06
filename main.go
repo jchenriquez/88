8c821560ed3abc48ad4b5be3a66dfddc6642703a
jchenriquez <jc.henriquez04@gmail.com>
2019-12-05 20:12:48 -0500

    publishing simple solution.

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+	currIndex := len(nums1) - 1
+	n--
+	m--
+
+	for m >= 0 && n >= 0 {
+		if nums1[m] > nums2[n] {
+			nums1[currIndex] = nums1[m]
+			m--
+		} else {
+			nums1[currIndex] = nums2[n]
+			n--
+		}
+
+		currIndex--
+	}
+
+}
+
+func main() {
+	arr1 := []int{1,2,3,0,0,0}
+	arr2 := []int{2,5,6}
+	merge(arr1, 3, arr2, 3)
+	fmt.Printf("merged %v\n", arr1)
+}
